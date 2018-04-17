7ec8d115a663d90a859771dd9fe8cfd02ef580a3
Eyads99 <eyadshaban99@gmail.com>
2018-04-17 13:14:45 +0200

    update to client

--- Client.cpp
+++ Client.cpp
@@ -3,24 +3,36 @@
 #include <string>
 
 
-typedef struct message{
+typedef struct message {
 
-  string sn;//sender name
-  string  rn;//recevier name
-  string subject;//message subject
-  string body;// the main body of text 
-                      } message_t
+	string sn;//sender name
+	string  rn;//recevier name
+	string subject;//message subject
+	string body;// the main body of text 
 
-void read_message()
+} message_t;
+
+using namespace std;
+
+void read_message(message_t)
 {
-
+	message_t m;
+	cout << "please input your name" << endl;
+	cin >> m.sn;
+	cout << "please input the recevier's name" << endl;
+	cin >> m.rn;
+	cout << "please input your subject title" << endl;
+	cin>> m.subject;// subject
+	cout << "Please give the body of your message"<<endl;
+	cin >> m.body;
 }
-int main ()
+
+int main()
 {
-  message_t m;
-  read_message (&m);
-  store_message_to_file ($m,"message.txt");
-  string reply = send_to_apache (“127.0.0.1”,”message.txt”);
-  cout << reply << endl;
-  remove_message("message.txt");
+	message_t m;
+	read_message(&m);
+	store_message_to_file($m, "message.txt");
+	string reply = send_to_apache(“127.0.0.1”, ”message.txt”);
+	cout << reply << endl;
+	remove_message("message.txt");
 }
