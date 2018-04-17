863cadc7f6c1834fe5c36f4fa050d6d685a73f9b
Eyads99 <eyadshaban99@gmail.com>
2018-04-17 14:39:32 +0200

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -2,19 +2,28 @@
 #include <fstream>
 #include <string>
 
+using namespace std;
 
 typedef struct message {
 
 	string sn;//sender name
-	string  rn;//recevier name
+	string rn;//recevier name
 	string subject;//message subject
 	string body;// the main body of text 
 
 } message_t;
 
-using namespace std;
 
-void read_message(message_t)
+void store_message_to_file(message_t m)
+{	
+	ofstream omessage;
+	omessage.open("text.txt");
+	//omessage.open("thefile.txt");
+	omessage << m.sn << endl << m.rn << endl << m.subject << endl << m.body << endl;
+	cout<< m.sn << endl << m.rn << endl << m.subject << endl << m.body << endl;
+}
+
+message_t read_message()
 {
 	message_t m;
 	cout << "please input your name" << endl;
@@ -25,14 +34,19 @@ void read_message(message_t)
 	cin>> m.subject;// subject
 	cout << "Please give the body of your message"<<endl;
 	cin >> m.body;
+	//cout << m.sn << endl << m.rn << endl << m.subject << endl << m.body << endl;
+	return m;
 }
+
 
 int main()
 {
 	message_t m;
-	read_message(&m);
-	store_message_to_file($m, "message.txt");
-	string reply = send_to_apache(“127.0.0.1”, ”message.txt”);
-	cout << reply << endl;
-	remove_message("message.txt");
+	m=read_message();
+	store_message_to_file(m);
+//	store_message_to_file($m, "message.txt");
+	//string reply = send_to_apache(“127.0.0.1”, ”message.txt”);
+	//cout << reply << endl;
+	//remove_message("message.txt");
+	system("pause");
 }
